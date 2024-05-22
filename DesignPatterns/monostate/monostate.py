4767f7672c3f185f39fd48021208242c56660e76
WilliamMezzina <william.mezzina@gmail.com>
2024-05-22 17:07:32 -0300

    Estudo 22/05/2024 Monostate-2

--- DesignPatterns/monostate/monostate.py
+++ DesignPatterns/monostate/monostate.py
@@ -15,18 +15,36 @@ class MonoStateSimple(StringReprMixin):
     def __init__(self):
         self.__dict__ = self._state
 
+class MonoState(StringReprMixin):
+    _state = {}
 
+    def __new__(cls, *args, **kwargs):
+        obj = super().__new__(cls)
+        obj.__dict__ = cls._state
+        return obj
+
+    def __init__(self, nome=None, sobrenome=None):
+        if nome is not None:
+            self.nome = nome
+        
+        if sobrenome is not None:
+            self.sobrenome = sobrenome
 
 
 if __name__ == '__main__':
-    m1 = MonoStateSimple()
-    m2 = MonoStateSimple()
+    ms1 = MonoStateSimple()
+    ms2 = MonoStateSimple()
 
+
+    print(ms1)
+    print(ms2)
+
+    ms1.x = 30
+
+    print(ms1)
+    print(ms2)
+
+    m1 = MonoState(nome="William")
+    m2 = MonoState(sobrenome="Mezzina")
 
     print(m1)
-    print(m2)
-
-    m1.x = 30
-
-    print(m1)
-    print(m2)
